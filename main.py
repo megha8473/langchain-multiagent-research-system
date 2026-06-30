ef5459fab5d357cf821f64a57a56472c640e45f5
Megha <meghamundra94@gmail.com>
2026-06-30 17:18:43 +0530

    final working flow

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.tools.tools import web_search,scrape_url
+from src.pipelines.pipeline import run_research_pipeline
 
-output = web_search.invoke("Recent advancements in AI research")
-print(output)
-
+# output = web_search.invoke("Recent advancements in AI research")
+# print(output)
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
 # scraped_content = scrape_url.invoke({"url": "https://www.crescendo.ai/news/latest-ai-news-and-updates"})
 # print(scraped_content)
